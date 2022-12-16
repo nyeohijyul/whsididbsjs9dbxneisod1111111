a98e2e45d4f73ae2b9c062aa5ed9d8c48b83b79b
nyeohijyul <119671625+nyeohijyul@users.noreply.github.com>
2022-12-16 10:58:00 +0900

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,294 @@
+var element_number = prompt("1~20입력")
+var response = prompt(element_number + "번 원소는?")
+if(element_number = 1) {
+    if(response = "수소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 2) {
+    if(response = "헬륨") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 3) {
+    if(response = "리튬") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 4) {
+    if(response = "베릴륨") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 5) {
+    if(response = "붕소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 6) {
+    if(response = "탄소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 7) {
+    if(response = "질소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 8) {
+    if(response = "산소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 9) {
+    if(response = "플루오린") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 10) {
+    if(response = "네온") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 11) {
+    if(response = "나트륨") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 12) {
+    if(response = "마그네슘") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 13) {
+    if(response = "알루미늄") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 14) {
+    if(response = "규소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 15) {
+    if(response = "인") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 16) {
+    if(response = "황") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 17) {
+    if(response = "염소") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 18) {
+    if(response = "아르곤") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 19) {
+    if(response = "칼륨") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(element_number = 20) {
+    if(response = "칼슘") {
+        var correct = o
+    } else {
+        var correct = x
+    }
+}
+if(correct = "o") {
+    var response2 = prompt("족과 주기는?", "1주기 2족이라면 (1, 2) 형식으로 입력하세요")
+} else {
+    alert("틀렸습니다")
+}
+}
+if(element_number = 1) {
+    if(response2 = "(1, 1)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 2) {
+    if(response2 = "(1, 18)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 3) {
+    if(response2 = "(2, 1)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 4) {
+    if(response2 = "(2, 2)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 5) {
+    if(response2 = "(2, 13)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 6) {
+    if(response2 = "(2, 14)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 7) {
+    if(response2 = "(2, 15)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 8) {
+    if(response2 = "(2, 16)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 9) {
+    if(response2 = "(2, 17)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 10) {
+    if(response2 = "(2, 18)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 11) {
+    if(response2 = "(3, 1)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 12) {
+    if(response2 = "(3, 2)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 13) {
+    if(response2 = "(3, 13)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 14) {
+    if(response2 = "(3, 14)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 15) {
+    if(response2 = "(3, 15)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 16) {
+    if(response2 = "(3, 16)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 17) {
+    if(response2 = "(3, 17)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 18) {
+    if(response2 = "(3, 18)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 19) {
+    if(response2 = "(4, 1)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(element_number = 20) {
+    if(response2 = "(4, 2)") {
+        var correct2 = o
+    } else {
+        var correct2 = x
+    }
+}
+if(correct2 = "o") {
+    alert("정답입니다.")
+} else {
+    alert("틀렸습니다.")
+}
+}
